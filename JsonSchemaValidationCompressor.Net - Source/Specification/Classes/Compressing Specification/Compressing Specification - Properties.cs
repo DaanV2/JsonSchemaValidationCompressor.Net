5da9b43a7e8d522e4eed1726d19787024a9ea9f8
DaanV2 <daanverstraten@hotmail.com>
2025-06-02 22:09:00 +0200

    fix

--- JsonSchemaValidationCompressor.Net - Source/Specification/Classes/Compressing Specification/Compressing Specification - Properties.cs
+++ JsonSchemaValidationCompressor.Net - Source/Specification/Classes/Compressing Specification/Compressing Specification - Properties.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DaanV2.Json.Specification {
     public sealed partial class CompressingSpecification {
         /// <summary>Gets or sets the file that are processed</summary>
         public List<FileSpecification> Files { get; set; }
+
+        /// <summary>Gets or sets the schema</summary>
+        [JsonProperty("$schema")]
+        public String Schema { get; set; }
     }
 }
